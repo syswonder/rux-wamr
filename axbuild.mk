f622f5f96fbcabffb8012cacf2df43f1017052a8
AuYang261 <459461160@qq.com>
2024-03-22 16:52:49 +0800

    update wamr to latest version(2024.3.22) and remove patch of ephemeral

--- axbuild.mk
+++ axbuild.mk
@@ -1,4 +1,4 @@
-wamr-version := a43018ff72eb3a177bed9200a42f54274dfcc850
+wamr-version := ca364eb5d7a42992748f4e3b57f2761995b9c2e4
 wamr-dir := $(APP)/wasm-micro-runtime-$(wamr-version)
 
 CMAKE = cmake
@@ -10,16 +10,12 @@ CXX_COMPILER := $(shell which $(CC))
 AR := $(shell which $(AR))
 RANLIB := $(shell which $(RANLIB))
 CROSS_COMPILE_PATH := $(shell dirname $(C_COMPILER))/..
-C_INCLUDE_DIR := -I$(CROSS_COMPILE_PATH)/$(ARCH)-linux-musl/include/ -I$(CROSS_COMPILE_PATH)/lib/gcc/$(ARCH)-linux-musl/11.2.1/include/
+C_INCLUDE_DIR := -isystem $(CROSS_COMPILE_PATH)/$(ARCH)-linux-musl/include/ -isystem $(CROSS_COMPILE_PATH)/lib/gcc/$(ARCH)-linux-musl/11.2.1/include/
 CPP_INCLUDE_DIR := -I$(CROSS_COMPILE_PATH)/$(ARCH)-linux-musl/include/c++/11.2.1/ -I$(CROSS_COMPILE_PATH)/$(ARCH)-linux-musl/include/c++/11.2.1/$(ARCH)-linux-musl/ $(C_INCLUDE_DIR)
 
-ifndef $(WASI_NN)
-WASI_NN = 0
-endif
-
-ifndef $(WASI_NN_LOG_LEVEL)
-WASI_NN_LOG_LEVEL = 1
-endif
+WASI_NN ?= 0
+WASI_NN_LOG_LEVEL ?= 1
+WAMR_BUILD_WASI_EPHEMERAL_NN ?= 0
 
 app-objs := wamr.o
 wamr_product_dir = $(wamr-dir)/product-mini/platforms/ruxos
@@ -31,23 +27,23 @@ $(wamr-dir):
 	tar -zxf $(APP)/$(wamr-version).tar.gz -C $(APP) && rm -f $(APP)/$(wamr-version).tar.gz
 	cd $(wamr-dir) && git init && git add .
 	patch -p1 -N -d $(wamr-dir) --no-backup-if-mismatch -r - < $(APP)/wamr.patch
-	patch -p1 -N -d $(wamr-dir) --no-backup-if-mismatch -r - < $(APP)/wasi_ephemeral_nn.patch
 
 $(APP)/$(app-objs): build_wamr
 build_wamr: $(wamr-dir) $(APP)/axbuild.mk
 	mkdir -p $(wamr_product_dir) && cp -r $(wamr_product_dir)/../linux/* $(wamr_product_dir) && cp $(APP)/CMakeLists.txt $(APP)/*.cmake $(wamr_product_dir)
-	cd $(wamr_product_dir) && mkdir -p build && cd build && \
+	mkdir -p $(wamr_product_build) && cd $(wamr_product_build) && \
 		$(CMAKE) -DCMAKE_TOOLCHAIN_FILE=../$(ARCH)_toolchain.cmake .. \
 			-DCMAKE_C_COMPILER=$(C_COMPILER) -DCMAKE_CXX_COMPILER=$(CXX_COMPILER) -DCMAKE_AR=$(AR) -DCMAKE_RANLIB=$(RANLIB) \
-			-DCMAKE_C_FLAGS="-DNN_LOG_LEVEL=$(WASI_NN_LOG_LEVEL) -D__builtin___clear_cache=// $(C_INCLUDE_DIR) $(CFLAGS)" \
-			-DCMAKE_CXX_FLAGS="-DNN_LOG_LEVEL=$(WASI_NN_LOG_LEVEL) -D\"strtoll_l(s, e, b, l)=strtoll(s, e, b)\" -D\"strtoull_l(s, e, b, l)=strtoull(s, e, b)\" $(CPP_INCLUDE_DIR) $(CFLAGS)" \
+			-DCMAKE_C_FLAGS="-DNN_LOG_LEVEL=$(WASI_NN_LOG_LEVEL) -D__builtin___clear_cache=// $(CFLAGS) $(C_INCLUDE_DIR) " \
+			-DCMAKE_CXX_FLAGS="-DNN_LOG_LEVEL=$(WASI_NN_LOG_LEVEL) -D\"strtoll_l(s, e, b, l)=strtoll(s, e, b)\" -D\"strtoull_l(s, e, b, l)=strtoull(s, e, b)\" $(CFLAGS) $(CPP_INCLUDE_DIR) " \
 			-DWAMR_BUILD_TARGET=$(ARCH_UPPER) \
 			-DWAMR_DISABLE_HW_BOUND_CHECK=1 \
 			-DWAMR_DISABLE_WRITE_GS_BASE=1 \
 			-DWAMR_BUILD_WASI_NN=$(WASI_NN) \
+			-DWAMR_BUILD_WASI_EPHEMERAL_NN=$(WAMR_BUILD_WASI_EPHEMERAL_NN) \
 			-DFLATBUFFERS_LOCALE_INDEPENDENT=1 \
 			-UMADV_HUGEPAGE && \
-		$(MAKE) -j
+		$(MAKE) -j17
 	mkdir -p $(wamr_product_build)/libgcc && cd $(wamr_product_build)/libgcc && \
 		ln -s -f $(CROSS_COMPILE_PATH)/lib/gcc/*-linux-musl/*/libgcc.a ./ && \
 		$(AR) x libgcc.a _clrsbsi2.o
@@ -73,8 +69,11 @@ else
 endif
 
 clean_c::
-	rm -f $(wamr_product_build)/
 	rm -rf $(wamr_product_build)/_deps
 	rm -rf $(wamr_product_build)/CMakeFiles
+	rm -rf $(wamr_product_build)/tmp
+	rm -rf $(wamr_product_build)/pthreadpool
+	rm -rf $(wamr_product_build)/flatbuffers-flatc
+	find $(wamr_product_build) -maxdepth 1 -type f -exec rm {} \;
 
 .PHONY: build_wamr clean_c
